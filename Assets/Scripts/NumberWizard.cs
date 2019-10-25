2ba15b942292fb41fbe280fd028d389c77082b9e
Paweł Lewarski <pawel.lewarski@akmf.pl>
2019-10-25 11:34:43 +0200

    lekcja 45

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    int guess;
+    [SerializeField] Text guessText;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        //guess = (max + min) / 2;
+
+        //guessText.text = "Is it " + guess.ToString() + "?";
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        min = guess - 1;
+        NextGuess();
+    }
+
+    public void OnPressHigher()
+    {
+        max = guess + 1;
+        NextGuess();
+    }
+    
+    void NextGuess()
+    {
+        //        guess = (max + min) / 2;
+        guess = Random.Range(min, max);
+        guessText.text = "Is it " + guess.ToString() + "?";
+    }
+}
